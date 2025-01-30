57f76bb01fee24ab99211e5fe5e0e4469b6ac0c0
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-30 09:04:55 +0200

    Non-Genaric Collection[Hashtable]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Demo
+﻿using System.Collections;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Non-Genaric Collection [Hashtable]
+            Hashtable Note = new Hashtable(new StringEqualityComparer());
+            Note.Add("Sajeda", 123);
+            Note.Add("Eman", 456);
+            Note.Add("Nada", 789);
+            foreach (DictionaryEntry i in Note)
+            {
+                Console.WriteLine($"The Key is {i.Key}, The Value is {i.Value}");
+            }
+            if (Note.ContainsKey("Yasmine"))
+            {
+                Console.WriteLine(Note["Yasmine"]);
+            }
+            #endregion
+
+           
         }
     }
 }
