ba0765394103e7540a2b0d4a03ccbfff64da15f3
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-30 13:20:18 +0200

    Part1

---
+++ Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part1
+            // in the Demo
+            #endregion
+
+        }
+    }
+}
